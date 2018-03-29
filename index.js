07bdb09d48e9e79a0be431bf09cef8b56891970e
Spokeek <spokeek@gmail.com>
2018-03-29 21:30:13 +0200

    added views

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 // important modules
 require('dotenv').config()
 require('colors')
+const uuid = require('uuid/v4')
 const {log, logLevels} = require(`${__dirname}/utils`)
 
 // constants
 const PORT = process.env.PORT || 3000
 const DEBUG_LEVEL = logLevels.includes(process.env.DEBUG_LEVEL) ? process.env.DEBUG_LEVEL : 'info' // not actually used for now
 
-//modules
+// modules express
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+app.use('/js-cookie', express.static(`${__dirname}/node_modules/js-cookie/src`))
+app.use('/public', express.static(`${__dirname}/public`))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
+// actual code
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+	
+    res.render('home', {title: "Client", uuid: uuid()})
 })
 
-app.listen(3000, function () {
+io.on('connection', (socket) => {
+  log.info('a user connected')
+})
+
+http.listen(PORT, () => {
     log.info(`Client app started on port ${PORT} .`)
 })
