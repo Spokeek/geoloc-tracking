1bc2e92fd2207b73bcb9b6b9eea7ec6f902a1105
alexandre.belhomme <alexandre.belhomme@capgemini.com>
2018-04-19 22:28:18 +0200

    WIP

--- front/src/index.js
+++ front/src/index.js
@@ -6,12 +6,14 @@ const {log, logLevels} = require(`${__dirname}/utils`)
 const redis = require('redis')
 
 // constants
-const PORT = process.env.PORT || 3000
+const PORT = +process.env.PORT || 3000
 const DEBUG_LEVEL = logLevels.includes(process.env.DEBUG_LEVEL) ? process.env.DEBUG_LEVEL : 'info' // not actually used for now
 const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
-const redisClient = redis.createClient({host: REDIS_HOST});
+const REDIS_SAVE_INTERVAL = +process.env.REDIS_SAVE_INTERVAL || 5//60 * 3 // In seconds
+const USER_EMIT_INFO = +process.env.USER_EMIT_INFO || 10 // In seconds
 
-// modules express
+// modules configuration
+const redisClient = redis.createClient({host: REDIS_HOST})
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
@@ -25,9 +27,7 @@ const io = require('socket.io')(http)
 // actual code
 app.get('/', (req, res) => {
 		res.render('home', {
-		title: "Client",
 		uuid: uuid(),
-		intervalUpdate: (15 * 1000) // in ms
 	})
 })
 
@@ -35,9 +35,35 @@ io.on('connection', (socket) => {
 	log.info('a user connected')
 	
 	socket.on('data', (data) => {
-		log.info(`user ${data.uuid} moved to "${data.latitude}:${data.longitude}"`)
+		const {lat, lng} =  data
+		log.debug(`user ${data.uuid} moved to "${lat}:${lng}"`)
+		
+		redisClient.sadd('uuids', data.uuid)
+		redisClient.lpush(data.uuid, `${+new Date()}|${lat}|${lng}`, redis.print)
 	})
 })
+
+
+setInterval(() => {
+	log.debug("Sending positions to users")
+	redisClient.smembers('uuids', (err, uuids) => {
+		log.debug(`uuids: ${uuids}`)
+		const m = redisClient.multi()
+		uuids.forEach((uuid) => m.lrange(uuid, -1, 1))
+		m.exec((err, replies) => {
+			log.info(`replies: ${replies}`)
+			
+			//.forEach((p,i) => log.info(`${uuids[i]} => at ${p.date} sur ${p.lat}:${p.lng}`))
+		})
+	})
+}, USER_EMIT_INFO * 1000)
+
+if(REDIS_SAVE_INTERVAL > 0){
+	setInterval(() => {
+		log.debug("Saving database")
+		redisClient.save()
+	}, REDIS_SAVE_INTERVAL * 1000)
+}
 
 http.listen(PORT, () => {
     log.info(`Client app started on port ${PORT} .`)
