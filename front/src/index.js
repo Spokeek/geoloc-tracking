5b85a84e6708303649c991780bc8cfca7dadf951
Spokeek <spokeek@gmail.com>
2018-04-20 04:53:20 +0200

    a lot of updates, with icons, filtering, etc

--- front/src/index.js
+++ front/src/index.js
@@ -2,15 +2,16 @@
 require('dotenv').config()
 require('colors')
 const uuid = require('uuid/v4')
-const {log, logLevels} = require(`${__dirname}/utils`)
+const {log, logLevels, chunkArray, distanceInKmBetweenEarthCoordinates} = require(`${__dirname}/utils`)
 const redis = require('redis')
 
 // constants
 const PORT = +process.env.PORT || 3000
 const DEBUG_LEVEL = logLevels.includes(process.env.DEBUG_LEVEL) ? process.env.DEBUG_LEVEL : 'info' // not actually used for now
 const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
-const REDIS_SAVE_INTERVAL = +process.env.REDIS_SAVE_INTERVAL || 5//60 * 3 // In seconds
-const USER_EMIT_INFO = +process.env.USER_EMIT_INFO || 10 // In seconds
+const REDIS_SAVE_INTERVAL = +process.env.REDIS_SAVE_INTERVAL || 60 * 0.5 // In seconds
+const USER_EMIT_INFO = +process.env.USER_EMIT_INFO || 2 // In seconds
+const MAP_DISPLAY_DISTANCE = +process.env.MAP_DISPLAY_DISTANCE || 50 // In kilometers
 
 // modules configuration
 const redisClient = redis.createClient({host: REDIS_HOST})
@@ -31,32 +32,86 @@ app.get('/', (req, res) => {
 	})
 })
 
+let sockets_list = []
 io.on('connection', (socket) => {
-	log.info('a user connected')
+	log.debug('a user connected')
+	
+	socket.on('disconnect', () => {
+		log.debug("user disconnected")
+		sockets_list = sockets_list.filter(s => s.socket !== socket)
+	})
 	
 	socket.on('data', (data) => {
 		const {lat, lng} =  data
 		log.debug(`user ${data.uuid} moved to "${lat}:${lng}"`)
 		
-		redisClient.sadd('uuids', data.uuid)
-		redisClient.lpush(data.uuid, `${+new Date()}|${lat}|${lng}`, redis.print)
-	})
-})
-
-
-setInterval(() => {
-	log.debug("Sending positions to users")
-	redisClient.smembers('uuids', (err, uuids) => {
-		log.debug(`uuids: ${uuids}`)
 		const m = redisClient.multi()
-		uuids.forEach((uuid) => m.lrange(uuid, -1, 1))
-		m.exec((err, replies) => {
-			log.info(`replies: ${replies}`)
-			
-			//.forEach((p,i) => log.info(`${uuids[i]} => at ${p.date} sur ${p.lat}:${p.lng}`))
-		})
+		m.sadd('uuids', data.uuid)
+		m.setnx(`username_${data.uuid}`, 'anonymous')
+		m.rpush(data.uuid, `${+new Date()}|${lat}|${lng}`, log.info)
+		m.exec((err) => log.error(err))
+		if(!sockets_list.find(s => s.uuid === data.uuid)){
+			sockets_list = [...sockets_list, {uuid: data.uuid, socket}]
+		}
 	})
-}, USER_EMIT_INFO * 1000)
+	
+	socket.on('changeUsername', (data) => {
+		const {username, uuid} = data
+		log.debug(`${uuid} changed his username to ${username}`)
+		redisClient.set(`username_${uuid}`, username)
+	})
+	
+	setInterval(() => {
+		redisClient.smembers('uuids', (err, uuids) => {
+			if(err){
+				log.error(err)
+			}
+			else if(uuids){
+				//log.debug(uuids)
+				const m = redisClient.multi()
+				uuids.forEach((uuid) => {
+					m.lrange(uuid, -1, -1)
+					m.get(`username_${uuid}`)
+				})
+				m.exec((err, replies) => {
+					replies = chunkArray(replies, 2)
+					//log.debug(replies)
+					const users_data = replies
+						.map((r, i) => {
+							//log.debug(r)
+							if(r){
+								let obj = r[0][0]
+								obj = obj.split('|')
+								return ({
+									uuid: uuids[i],
+									date: new Date(obj[0] * 1000),
+									lat: obj[1],
+									lng: obj[2],
+									username: r[1]
+								})
+							}
+							else{
+								return null
+							}
+						})
+						.filter(p => p !== null)
+					users_data.forEach((p) => log.debug(`${p.uuid} => at ${p.date} sur ${p.lat}:${p.lng} par ${p.username}`))
+					sockets_list.forEach(s => {
+						const {lat, lng} = users_data.find(ud => ud.uuid === s.uuid)
+						const users_data_filtred = users_data
+							.filter(ud => distanceInKmBetweenEarthCoordinates(lat, lng, ud.lat, ud.lng) <= MAP_DISPLAY_DISTANCE)
+						log.debug(`for ${s.uuid}, we send ${users_data_filtred.length} data`)
+						s.socket.emit('users_data', users_data_filtred)
+					})
+				})
+			}
+			else{
+				log.debug("uuids empty")
+			}
+		})
+	}, USER_EMIT_INFO * 1000)
+
+})
 
 if(REDIS_SAVE_INTERVAL > 0){
 	setInterval(() => {
--- front/src/utils.js
+++ front/src/utils.js
@@ -8,7 +8,34 @@ const logLevels = {
 const log = Object.keys(logLevels)
 	.reduce((acc, curr) => ({
 		...acc,
-		[curr]: (message) => console.log(`[${curr}] ${message}`[logLevels[curr]])
+		[curr]: (message) => console.log(`[${curr}] ${typeof message == 'object' ? JSON.stringify(message) : message}`[logLevels[curr]])
 	}), {})
 
-module.exports = {log, logLevels: Object.keys(logLevels)}
+const chunkArray = (myArray, chunk_size) => {
+    var results = [];
+    
+    while (myArray.length) {
+        results.push(myArray.splice(0, chunk_size))
+    }
+    
+    return results
+}
+
+const distanceInKmBetweenEarthCoordinates = (lat1, lon1, lat2, lon2) => {
+	const degreesToRadians = (degrees) => degrees * Math.PI / 180
+
+	const earthRadiusKm = 6371
+	const dLat = degreesToRadians(lat2-lat1)
+	const dLon = degreesToRadians(lon2-lon1)
+
+	lat1 = degreesToRadians(lat1)
+	lat2 = degreesToRadians(lat2)
+
+	const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+	return earthRadiusKm * c
+}
+
+
+
+module.exports = {log, logLevels: Object.keys(logLevels), chunkArray, distanceInKmBetweenEarthCoordinates}
